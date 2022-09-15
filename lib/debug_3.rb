248aab47756e1f3fa66afeb1602173bd64255226
Thomas Allen <tomallen@MASTUDENT-Thomas-Allen-C02G4TXEQ6L4.local>
2022-09-15 11:24:41 +0100

    phase 2.4 debugging challenge

---
+++ lib/debug_3.rb
@@ -0,0 +1,16 @@
+
+
+def get_most_common_letter(text)
+  counter = Hash.new(0)
+  text.delete(' ').chars.each do |char|
+    counter[char] += 1
+  end
+  #p counter
+  counter.to_a.sort_by { |k, v| v }.reverse![0][0]
+end
+
+#puts "'#{get_most_common_letter("the roof, the roof, the roof is on fire!")}'"
+# Intended output:
+# 
+# > get_most_common_letter("the roof, the roof, the roof is on fire!")
+# => "o"
